3b4bdc7a08a2052d6fbf45c8472b804d53be80c3
Dani <daniafriyadi@gmail.com>
2020-02-23 20:43:30 +0700

    Fix router error

--- public/js/app.js
+++ public/js/app.js
@@ -30860,6 +30860,7 @@ __webpack_require__.r(__webpack_exports__);
 
 vue__WEBPACK_IMPORTED_MODULE_0___default.a.use(vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]);
 /* harmony default export */ __webpack_exports__["default"] = (new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
+  mode: 'history',
   linkExactActiveClass: 'active',
   routes: [{
     path: '/',
