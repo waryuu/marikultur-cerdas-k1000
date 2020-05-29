d0b2905f5fee3ac5311dd82016e5e88d1b940bdd
Dani Afriyadi <daniafriyadi@gmail.com>
2020-05-29 08:34:47 +0700

    update index.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,29 +4,26 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="apple-touch-icon" sizes="57x57" href="/img/icons/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="/img/icons/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="/img/icons/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="/img/icons/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="/img/icons/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="/img/icons/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="/img/icons/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="/img/icons/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="/img/icons/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192"  href="/img/icons/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/img/icons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="/img/icons/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/img/icons/favicon-16x16.png">
-        <link rel="manifest" href="/manifest.json">
+        <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('img/icons/apple-icon-57x57.png') }}">
+        <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('img/icons/apple-icon-60x60.png') }}">
+        <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('img/icons/apple-icon-72x72.png') }}">
+        <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/icons/apple-icon-76x76.png') }}">
+        <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('img/icons/apple-icon-114x114.png') }}">
+        <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('img/icons/apple-icon-120x120.png') }}">
+        <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('img/icons/apple-icon-144x144.png') }}">
+        <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('img/icons/apple-icon-152x152.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/icons/apple-icon-180x180.png') }}">
+        <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('img/icons/android-icon-192x192.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/icons/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('img/icons/favicon-96x96.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/icons/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('manifest.json') }}">
         <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+        <meta name="msapplication-TileImage" content="{{ asset('ms-icon-144x144.png') }}">
         <meta name="theme-color" content="#5e72e4">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Marikultur Cerdas K1000</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
-        {{-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"> --}}
-        {{-- <link rel="stylesheet" href="/css/all.min.css"> --}}
-        <!-- <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY"></script> -->
     </head>
     <body class="{{ $class ?? '' }}">
         <noscript>
@@ -35,19 +32,10 @@
         <div id="app"></div>
         <!-- built files will be auto injected -->
         <!-- Argon JS -->
-        <script src="{{ asset('js/app.js') }}"></script>
         <script>
-            if ('serviceWorker' in navigator ) {
-              window.addEventListener('load', function() {
-                  navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-                      // Registration was successful
-                      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-                  }, function(err) {
-                      // registration failed :(
-                      console.log('ServiceWorker registration failed: ', err);
-                  });
-              });
-          }
+            var flagsUrl = '{{ asset('service-worker.js') }}';
         </script>
+        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ asset('js/service.js') }}"></script>
     </body>
 </html>
